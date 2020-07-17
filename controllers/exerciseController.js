33fa30b3064a7acabd79190e3a6e73c87c6540a6
Sam Dratch <dratch.sam1@gmail.com>
2020-07-17 14:11:23 -0400

    added put request into exercise

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -4,8 +4,9 @@ const db = require("../models");
 
 // /api/exercise
 
-//get routes for exercises
+//get routes for all exercises (might need to show o)
 router.get("/", (req, res) => {
+  //return all exercises
   db.Exercise.findAll({}).then((result) => {
     res.json(result);
   });
@@ -14,6 +15,7 @@ router.get("/", (req, res) => {
 //request to create a new exercise
 router.post("/", (req, res) => {
   db.Exercise.create(req.body)
+    //on sucess send a response of the data and a message that it was created
     .then((result) => {
       res.json({
         error: false,
@@ -21,6 +23,7 @@ router.post("/", (req, res) => {
         message: "Successfully created new exercise",
       });
     })
+    //on failure send a status code with message of unable to be created
     .catch((err) => {
       console.log(err);
       res.status(500).json({
@@ -30,13 +33,60 @@ router.post("/", (req, res) => {
       });
     });
 });
+//request for updating an exercise
+router.put("/:id", (req, res) => {
+  //update the name, reps, sets, and weights part of the exercise
+  db.Exercise.update(req.body,
+    {
+      //location of the exercise that is being updated
+      where: {
+        id: req.params.id,
+      },
+    }
+    //on sucess send a response of the data and a message that it was updated
+  )
+    .then((result) => {
+      res.json({
+        error: false,
+        data: result,
+        message: "Succesfully updated exercise",
+      });
+      //on failure send a status code with message of unable to be updated
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to update exercise",
+      });
+    });
+});
+
+//request for deleting an exercise
 router.delete("/:id", (req, res) => {
+  //call to destroy the exercise
   db.Exercise.destroy({
+    //location of which exercise to destroy
     where: {
       id: req.params.id,
     },
+    //on sucess send a response of the data and a message that it was deleted
   }).then((result) => {
-    res.json(result);
+    res
+      .json({
+        error: true,
+        data: result,
+        message: "Successfully deleted exercise",
+      })
+      //on failure send a status code with message of unable to be deleted
+      .catch((err) => {
+        res.status(500),
+          json({
+            error: true,
+            data: null,
+            message: "Unable to delete exercise",
+          });
+      });
   });
 });
 module.exports = router;
