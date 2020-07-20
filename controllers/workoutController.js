8be188fba10cb7d3142129362c8c51ffe48fdcba
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-07-20 11:52:22 -0400

    committing to be able to pull

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -3,71 +3,62 @@ const router = express.Router();
 const db = require("../models");
 
 // Route to display the index page
-router.get("/api/workouts", function(req, res) {
-    db.Workout.findAll(function(data) {
-      let hbsObject = {
-        Workout: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+router.get("/workouts", function(req, res) {
+    db.Workout.findAll({})
+    .then(function(result) {
+      res.render("index", result);
     });
   });
 
 // Route that shows one workout
-router.get("/api/workouts/:id", function(req, res) {
+router.get("/workouts/:id", function(req, res) {
     db.Workout.findOne({
       where: {
         id: req.params.id
-      }
+      },
+      include: [db.Excercise]   
     })
-      .then(function(dbWorkout) {
-        res.json(dbWorkout);
+      .then(function(result) {
+        res.render("index", result);
       });
   });
   
 //  Route that creates new workout
 router.post("/api/workouts/", function (req, res) {
-  db.Workout.create(["name", "type"], [req.body.name, req.body.type], function (
-    result
-  ) {
-    // This needs to update depending on the handlebars page(maybe workouts handlebars page), not index.
-    res.render("index", result);
-  });
+    db.Workout.create(req.body)
+        .then(function(result) {
+          res.render("index", result);
+        });
 });
+
 
 // Route that takes you to the workout user selected by id
 router.put("/api/workouts/:id", function (req, res) {
-  let condition = "id = " + req.params.id;
-
-  Workout.update(
-    {
-      type: req.body.type,
-    },
-    condition,
-    function (result) {
-      if (result.changedRows == 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    }
-  );
+    db.Workout.update(req.body,
+        {
+          where: {
+            type: req.body.type,
+            id: req.body.id
+          }
+        })
+        .then(function(result) {
+          res.json(result);
+        });
 });
 
 // Route that deletes workouts by their id's
 router.delete("/api/workouts/:id", function(req, res) {
-    let condition = "id = " + req.params.id;
-  
-    db.Workout.delete(condition, function(result) {
-      if (result.affectedRows == 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+    db.Workout.delete({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(function(result) {
+          res.json(result);
+        });
   });
 
 
 module.exports = router;
 
-// I really don't know if any of this is right
+
